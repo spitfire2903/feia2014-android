34758b25da5996f6016cbe2ff51d01eac4021173
Ricardo Nunes de Miranda <nm.ricardo@gmail.com>
2014-09-18 07:25:59 -0300

    comp

---
+++ FEIA2014/src/br/com/sevencode/android/feia2014/CalendarFragment.java
@@ -0,0 +1,9 @@
+package br.com.sevencode.android.feia2014;
+
+public class CalendarFragment extends BaseFragment {
+
+	public CalendarFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
